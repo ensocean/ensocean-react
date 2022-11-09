d9a00b4943086ee6d8910a3dc4666c812b4a1566
Ramazan Dönmez <ramazan.dnmz@gmail.com>
2022-11-09 17:08:39 +0300

    fix

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,8 +1,10 @@
+import React, {useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./partials/Footer";
 import Navbar from "./partials/Navbar";
+import ReactGA from "react-ga4"
 
-const Layout = () => {
+const Layout = () => { 
   return (
     <>
       <Navbar />
