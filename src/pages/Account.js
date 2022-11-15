54eca5f23923019f7b45610d861ae9b5efd86d05
Ramazan Dönmez <ramazan.dnmz@gmail.com>
2022-11-15 14:20:09 +0300

    changes.

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,25 +1,30 @@
 import {Helmet} from "react-helmet";
+import { useParams } from "react-router-dom";
+import { obscureAddress } from "../helpers/String";
+import Filter from "./partials/Filter";
 
 const Account = () => {
+    const { address } = useParams();  
+    let where = { label_not: null, owner: address }; 
     return (
-        <>  
-        <Helmet> 
-              <title>{"AccountAddress"} - EnsOcean</title>
-              <meta name="description" content="" />
-        </Helmet>
-        <div className="container-fluid bg-primary">
-            <div className="container text-center p-3 text-white">
-                <h1>Account</h1>
-            </div> 
-        </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-                
-             </div>
-          </div>
-        </div>
-      </>
+      <>
+      <Helmet> 
+          <title>{address} - EnsOcean</title>
+          <meta name="description" content="" />
+      </Helmet>
+      <div className="container-fluid bg-primary">
+          <div className="container text-center p-3 text-white">
+              <h1>{obscureAddress(address)}</h1>
+          </div> 
+      </div>
+      <div className="container-fluid p-0 m-0">
+            <div className="card text-center">
+                <div className="card-body">
+                <Filter First={100} Skip={0} OrderBy={"created"} OrderDirection={"desc"} Where={where} />
+                </div>
+            </div>
+        </div>  
+      </> 
     );
 };
   
--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,44 +1,98 @@
-import React from "react"; 
+import React, { useEffect, useState } from "react"; 
 import { useLocation  } from "react-router-dom";
 import Filter from "./partials/Filter";
 import moment from 'moment'; 
+import {Helmet} from "react-helmet"; 
+import { Link } from "react-router-dom";
 
 const GRACE_PERIOD = Number(process.env.REACT_APP_GRACE_PREIOD);
 const PREMIUM_PERIOD =  Number(process.env.REACT_APP_PREMIUM_PERIOD);
 
+let where = { label_not: null };
+let pageTitle = "Browse";
+let orderBy = "created";
+let orderDirection = "desc";
+
 const Discover = () => { 
     const location = useLocation();
-    const query = new URLSearchParams(location.search);  
-    const tab = query.get("tab");  
-    let where = { label_not: null }; 
 
-    if(tab === "expired") { 
-        where.expires_lte = moment().add(-GRACE_PERIOD, "days").add(-PREMIUM_PERIOD, "days").utc().unix(); 
-        return (
-            <Filter PageTitle={"Dropped Domains"} First={100} Skip={0} Tab={tab} OrderBy={"expires"} OrderDirection={"desc"} Where={where} />
-        )
-    } else if(tab === "expiring") { 
-        where.expires_lte = moment().utc().unix();
-        where.expires_gte = moment().add(-PREMIUM_PERIOD, "days").utc().unix();
-        return ( 
-            <Filter PageTitle={"Expiring Soon"} First={100} Skip={0} Tab={tab} OrderBy={"expires"} OrderDirection={"asc"} Where={where} />
-        )
+    const query = new URLSearchParams(location.search);  
+    const tab = query.get("tab");
+  
+    if(tab === "expired") {  
+        pageTitle = "Recently Expired";
+        where = { 
+            label_not: null,
+            expires_lte: moment().add(-GRACE_PERIOD, "days").add(-PREMIUM_PERIOD, "days").utc().unix() 
+        }
+    } else if(tab === "expiring") {  
+        pageTitle = "Expiring Soon";
+        where = { 
+            label_not: null,
+            expires_lte: moment().utc().unix(),
+            expires_gte: moment().add(-PREMIUM_PERIOD, "days").utc().unix()
+        };
     } else if(tab === "premium") { 
-        where.expires_lte = moment().add(-GRACE_PERIOD, "days").utc().unix();
-        where.expires_gte = moment().add(-GRACE_PERIOD, "days").add(-PREMIUM_PERIOD, "days").utc().unix();
-        return ( 
-            <Filter PageTitle={"Premium Right Now"} First={100} Skip={0} Tab={tab} OrderBy={"expires"} OrderDirection={"asc"} Where={where} />
-        )
-    } else if(tab === "registered") {
-        where.registered_not = null;
-        return (
-            <Filter PageTitle={"Recently Registered"} First={100} Skip={0} Tab={tab} OrderBy={"registered"} OrderDirection={"desc"} Where={where} />
-        )
+        pageTitle = "Premium Right Now";
+        where = { 
+            label_not: null,
+            expires_lte: moment().add(-GRACE_PERIOD, "days").utc().unix(),
+            expires_gte: moment().add(-GRACE_PERIOD, "days").add(-PREMIUM_PERIOD, "days").utc().unix()
+        }
+    } else if(tab === "registered") { 
+        pageTitle = "Recently Registered";
+        where = { 
+            label_not: null,
+            registered_not: null
+        }
     } else {
-        return (
-            <Filter PageTitle={"Browse ENS Domains"} First={100} Skip={0} Tab={tab} OrderBy={"created"} OrderDirection={"desc"} Where={where} />
-        )
-    } 
+        pageTitle = "Browse";
+        where = { 
+            label_not: null
+        }
+    }
+
+    return (
+        <>
+        <Helmet> 
+            <title>Browse Ethereum Name Service (ENS) Domains - EnsOcean</title>
+            <meta name="description" content="Browse Ethereum Name Service (ENS) domains easily. Find your web3 domain name idea. " />
+        </Helmet>
+        <div className="container-fluid bg-primary">
+            <div className="container text-center p-3 text-white">
+                <h1>{pageTitle}</h1>
+            </div> 
+        </div>
+        <div className="container-fluid p-0 m-0">
+            <div className="card text-center">
+                <div className="card-header border-0">
+                    <ul className="nav nav-tabs card-header-tabs">
+                        <li className="nav-item">
+                            <Link className={"nav-link fs-5 p-3 "+ (tab === "all" ? "active": "") } to="/discover?tab=all">All</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className={"nav-link fs-5 p-3 "+ (tab === "expired" ? "active": "")} to="/discover?tab=expired">Expired</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className={"nav-link fs-5 p-3 "+ (tab === "expiring" ? "active": "")} to="/discover?tab=expiring">Expiring</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className={" nav-link fs-5 p-3 "+ (tab === "premium" ? "active": "")} to="/discover?tab=premium">Premium</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className={"nav-link fs-5  p-3 "+ (tab === "registered" ? "active": "")} to="/discover?tab=registered">Recently Registered</Link>
+                        </li> 
+                    </ul> 
+                </div>
+                <div className="card-body">
+                    <Filter First={100} Skip={0} Tab={tab} OrderBy={orderBy} OrderDirection={orderDirection} Where={where} View="gallery" />
+                </div>
+            </div>
+        </div> 
+        </>
+    ) 
 };
+
+ 
   
 export default Discover;
