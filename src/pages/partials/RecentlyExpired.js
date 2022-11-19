2c2c0b47a57b6325933b829eb12754a2d2962b69
Ramazan Dönmez <ramazan.dnmz@gmail.com>
2022-11-19 12:27:01 +0300

    changes.

--- src/pages/partials/RecentlyExpired.js
+++ src/pages/partials/RecentlyExpired.js
@@ -36,6 +36,7 @@ const RECENTLY_EXPIRED = gql`
 
 
 const RecentExpired = () => {
+  
     const [getExpired, { called, loading, error, data, refetch }] = useLazyQuery(RECENTLY_EXPIRED,
       {
         variables: {  },
@@ -63,7 +64,80 @@ const RecentExpired = () => {
               </button>
           </div> 
           <ol className="list-group list-group-flush placeholder-glow">
-            <GetExpired called={called} data={data} loading={loading} error={error} />
+
+            { loading && 
+              <>
+                  {[...Array(10)].map((x, i) =>
+                  <li key={i} className="list-group-item p-3 fs-5 placeholder-glow justify-content-between d-flex">
+                      <span className="placeholder col-4"></span>
+                      <span className="placeholder col-2"></span>
+                  </li>
+                  )}
+              </>     
+            }
+
+            { error && 
+                <li className="list-group-item p-3 fs-5 placeholder-glow justify-content-between d-flex"><span className='text-danger'>{error.message}</span></li>
+            }
+
+            { data && data.domains.length < 1 &&
+                <li className="list-group-item p-3 fs-5 placeholder-glow justify-content-between d-flex"><span className='text-warning'>No Result</span></li>
+            }
+
+            { data && data.domains.length > 0 &&
+              <> 
+                  {data.domains.map((domain) => (
+                  <li key={domain.id} className="list-group-item list-group-item-action p-3">
+                      <div className="d-flex">
+                        <div className="flex-shrink-0">
+                          <div className="card text-start">
+                              <LazyLoadImage
+                                  alt={domain.name} 
+                                  className="img-fluid card-img-top card-img-bottom"
+                                  onError={(e)=> { e.target.src = notAvailable; }}
+                                  placeholder={<img src={spinner} className="img-fluid" />}
+                                  placeholderSrc={spinner}
+                                  width={46}
+                                  height={46}
+                                  src={ENS_IMAGE_URL.replace("{REACT_APP_ENS_REGISTRAR_ADDRESS}", ENS_REGISTRAR_ADDRESS).replace("{TOKEN_ID}", getTokenId(domain.label)) }
+                                /> 
+                            </div>
+                        </div>
+                        <div className="flex-grow-1 ms-3">
+                            <div className="d-flex flex-column flex-md-row justify-content-between">
+                                <Link
+                                    className="text-decoration-none link-dark fs-5 fw-bold" 
+                                    data-bs-toggle="tooltip" 
+                                    data-bs-title={"View "+ domain.name +" on EnsOcean"}
+                                    title={"View "+ domain.name +" on EnsOcean"}
+                                    to={encodeURIComponent(domain.name)}>
+                                    {obscureLabel(domain.label, 20)}.{domain.extension || "eth"}
+                                    { ' ' }
+                                    { (domain.tags.includes("include-unicode") || domain.tags.includes("only-unicode")) && 
+                                          <span data-bs-toogle="tooltip" data-bs-title="Include unicode characters">
+                                              <img src={exclamationTriangleFill} alt= "" />
+                                          </span>
+                                      }
+                                      &nbsp;
+                                      { !isValidName(domain.label) && 
+                                          <span data-bs-toogle="tooltip" data-bs-title="This domain is malformed!">
+                                              <img src={dashCircleFill} alt= ""  />
+                                          </span>
+                                      }
+                                </Link> 
+                                <small className="float-end text-success mt-2 mt-lg-0">
+                                    { isExpired(domain.expires) &&
+                                      <span className="text-success"> Available since {getExpires(domain.expires, true)} </span>
+                                    } 
+                                </small>
+                            </div>
+                        </div>
+                      </div> 
+                  </li>
+                ))}
+              </>
+            }
+
           </ol> 
           <div className="card-footer">
               <Link className="btn btn-success" to="/discover?tab=expired" title="View all expired ENS domains">View More</Link>
@@ -72,82 +146,5 @@ const RecentExpired = () => {
       </>
     )
 }
-
-const GetExpired = ({called, data, loading, error }) => {
-   
-
-    if(loading) {
-        return ( 
-            <>
-                {[...Array(10)].map((x, i) =>
-                <li key={i} className="list-group-item p-3 fs-5 placeholder-glow justify-content-between d-flex">
-                    <span className="placeholder col-4"></span>
-                    <span className="placeholder col-2"></span>
-                </li>
-                )}
-            </>     
-        )
-    } else if (error) {
-      return <li className="list-group-item p-3 fs-5 placeholder-glow justify-content-between d-flex"><span className='text-danger'>{error.message}</span></li>
-    } else {
-        return (
-            <>
-                {data.domains.length < 1 &&
-                  <li className="list-group-item p-3 fs-5 placeholder-glow justify-content-between d-flex"><span className='text-warning'>No Result</span></li>
-                }
-                {data.domains.map((domain) => (
-                <li key={domain.id} className="list-group-item list-group-item-action p-3">
-
-                    <div className="d-flex">
-                      <div className="flex-shrink-0">
-                        <div className="card h-100 text-start">
-                            <LazyLoadImage
-                                alt={domain.name} 
-                                className="img-fluid"
-                                onError={(e)=> { e.target.src = notAvailable; }}
-                                placeholder={<img src={spinner} className="img-fluid" />}
-                                placeholderSrc={spinner}
-                                width={46}
-                                height={46}
-                                src={ENS_IMAGE_URL.replace("{REACT_APP_ENS_REGISTRAR_ADDRESS}", ENS_REGISTRAR_ADDRESS).replace("{TOKEN_ID}", getTokenId(domain.label)) }
-                              /> 
-                          </div>
-                      </div>
-                      <div className="flex-grow-1 ms-3">
-                          <div className="d-flex flex-column flex-md-row justify-content-between">
-                              <Link
-                                  className="text-decoration-none link-dark fs-5 fw-bold" 
-                                  data-bs-toggle="tooltip" 
-                                  data-bs-title={"View "+ domain.name +" on EnsOcean"}
-                                  title={"View "+ domain.name +" on EnsOcean"}
-                                  to={encodeURIComponent(domain.name)}>
-                                  {obscureLabel(domain.label, 20)}.{domain.extension || "eth"}
-                                  { ' ' }
-                                  { (domain.tags.includes("include-unicode") || domain.tags.includes("only-unicode")) && 
-                                        <span data-bs-toogle="tooltip" data-bs-title="Include unicode characters">
-                                            <img src={exclamationTriangleFill} alt= "" />
-                                        </span>
-                                    }
-                                    &nbsp;
-                                    { !isValidName(domain.label) && 
-                                        <span data-bs-toogle="tooltip" data-bs-title="This domain is malformed!">
-                                            <img src={dashCircleFill} alt= ""  />
-                                        </span>
-                                    }
-                              </Link> 
-                              <small className="float-end text-success mt-2 mt-lg-0">
-                                  { isExpired(domain.expires) &&
-                                    <span className="text-success"> Available since {getExpires(domain.expires, true)} </span>
-                                  } 
-                              </small>
-                          </div>
-                      </div>
-                    </div> 
-                </li>
-              ))}
-            </>
-        )
-    } 
-}
-
+ 
 export default RecentExpired;
